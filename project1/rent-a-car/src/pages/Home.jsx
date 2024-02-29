60f329657fd721edd4e88dab4e09be62dc198ebe
bekirbali <bfbali43@gmail.com>
2024-02-29 23:55:17 +0300

    Add styles to Home component

--- project1/rent-a-car/src/pages/Home.jsx
+++ project1/rent-a-car/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Landing from "../components/Landing";
+import styles from "../styles/Home.module.scss";
 
 const Home = () => {
   return (
-    <div>
+    <div className={styles.home}>
       <Landing />
     </div>
   );
